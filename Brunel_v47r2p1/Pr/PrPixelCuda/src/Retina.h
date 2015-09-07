87276d98d2d9764679f7d7bba480a853a5ed56d8
towelenee <belous.mikhail@gmail.com>
2015-09-07 14:21:47 +0200

    update retina + get rid of PrCuda

--- Brunel_v47r2p1/Pr/PrPixelCuda/src/Retina.h
+++
@@ -1,8 +0,0 @@
-#pragma once
-
-#include <vector>
-#include <stdint.h>
-
-int cpuRetinaInvocation(
-    const std::vector<const std::vector<uint8_t>* > & input,
-    std::vector<std::vector<uint8_t> > & output);
